998d16f3bd2b73dadf8d6b46fd6a27f58f29cc5c
tinylic <lic.tiny@gmail.com>
2016-03-06 15:20:20 +0800

    6th Mar

--- cluster.h
+++ cluster.h
@@ -1,141 +1,127 @@
+#ifndef TFPROJECT_CLUSTER_H_
+#define TFPROJECT_CLUSTER_H_
+#include <WordLibrary.h>
 #include <cstdio>
 #include <cstring>
 #include <cmath>
-#include "vectors.h"
 
 using namespace std;
-WordEmbedding *mWordEmbedding;
-struct cluster{
+//WordEmbedding *mWordEmbedding;
 
-private:
-	unsigned clcn, iter, closeid;
-	// clcn : class number
+struct cluster {
+
+protected:
+	unsigned numClusters, numIterations;
+	// clcn : specified number of clusters
+	// iter : specified number of iterations for K-means
+
+	unsigned closeid;
 	// closeid : closest cluster
 
 	int *centcn;
 	int *cl;
-	real closev,x;
+
+	real closev, x;
 	real *cent;
 	// cent : coordinates of the cluster centroid
-	AllEmbeds vectors;
-	vector<int> IDvectors;
+
+	int vec_size;
+	AllEmbeds& vectors;
+
+	//vector<int> IDvectors;
 public:
 
-	void Init() {
-		mWordEmbedding = new WordEmbedding;
-		vectors.clear();
-		IDvectors.clear();
+
+
+	cluster(unsigned nClusters, unsigned iterations, AllEmbeds& vecEmbeddings) :
+			numClusters(nClusters), numIterations(iterations), vectors(
+					vecEmbeddings) {
+
+		vec_size = (int) vectors.size();  //the number of embeddings
+
+		cout << "vec_size == " << vec_size << endl;
+		centcn = new int[numClusters];
+		cl = new int[vec_size];
+		cent = new real[numClusters * layer1_size];
+		if (cent == NULL)
+			perror("Memory fail\n");
+		Kmeans();
 	}
 
-	cluster(){
-		// Initialization
-		Init();
+	~cluster() {
+		delete centcn;
+		delete cent;
+		delete cl;
 	}
 
-	~cluster(){
-		//delete mWordEmbedding;
-		//delete centcn;
-		//delete cent;
-	}
+	void Kmeans() {
+		// Run K-means on the word vectors
+		unsigned a, b, c, d;
+		cout << "Kmeans" << endl;
+		for (a = 0; a < vec_size; a++)
+			cl[a] = a % numClusters;
 
-	cluster(WordEmbedding *mEmbedding) {
-		Init();
-		mWordEmbedding = mEmbedding;
-	}
-	cluster(const char *fn, bool IsBinary) {
-		Init();
-		(*mWordEmbedding) = WordEmbedding(fn, IsBinary);
-	}
-	void Kmeans(int classes){
-			// Run K-means on the word vectors
-			unsigned a, b, c, d;
-			clcn = classes, iter = 10;
-			unsigned vec_size = (int)vectors.size();
-			cout << "vec_size == " << vec_size << endl;
-			//cout << clcn << endl;
-			cout << "Kmeans" << endl;
-			centcn = new int[classes];
-			cl = new int[vec_size];
-			cent = new real[classes * layer1_size];
-			if (cent == NULL) perror("Memory fail\n");
-			for (a = 0; a < vec_size; a++) cl[a] = a % clcn;
-			for (a = 0; a < iter; a++) {
-				memset(cent, 0, sizeof cent);
-				memset(centcn, 0, sizeof centcn);
-				for (b = 0; b < clcn; b++) centcn[b] = 1;
-				for (c = 0; c < vec_size; c++) {
-					for (d = 0; d < layer1_size; d++)
-						cent[layer1_size * cl[c] + d] += vectors[c][d];
-					centcn[cl[c]]++;
-				}
-				for (b = 0; b < clcn; b++) {
-					closev = 0;
-					for (c = 0; c < layer1_size; c++) {
-						cent[layer1_size * b + c] /= centcn[b];
-						closev += cent[layer1_size * b + c] * cent[layer1_size * b + c];
-					}
-					closev = sqrt(closev);
-					for (c = 0; c < layer1_size; c++) cent[layer1_size * b + c] /= closev;
-				}
-				for (c = 0; c < vec_size; c++) {
-					closev = -10;
-					closeid = 0;
-					for (d = 0; d < clcn; d++) {
-						x = 0;
-						for (b = 0; b < layer1_size; b++)
-							x += cent[layer1_size * d + b] * vectors[c][b];
-						if (x > closev) {
-							closev = x;
-							closeid = d;
-						}
-					}
-					cl[c] = closeid;
-				}
+		for (a = 0; a < numIterations; a++) {
+			for (b = 0; b < numClusters; b++)
+				for (c = 0; c < layer1_size; c++)
+					cent[b * layer1_size + c] = 0;
+			for (b = 0; b < numClusters; b++)
+				centcn[b] = 1;
+			for (c = 0; c < vec_size; c++) {
+				for (d = 0; d < layer1_size; d++)
+					cent[layer1_size * cl[c] + d] += vectors[c][d];
+				centcn[cl[c]]++;
 			}
-			for (a = 0; a < vec_size; a++)
-				mWordEmbedding -> ChangeEmbedWordCl(IDvectors[a], cl[a]);
-			cout << "End Kmeans" << endl;
+			for (b = 0; b < numClusters; b++) {
+				closev = 0;
+				for (c = 0; c < layer1_size; c++) {
+					cent[layer1_size * b + c] /= centcn[b];
+					closev += cent[layer1_size * b + c]
+							* cent[layer1_size * b + c];
+				}
+				closev = sqrt(closev);
+				for (c = 0; c < layer1_size; c++)
+					cent[layer1_size * b + c] /= closev;
+			}
+			for (c = 0; c < vec_size; c++) {
+				closev = -10;
+				closeid = 0;
+				for (d = 0; d < numClusters; d++) {
+					x = 0;
+					for (b = 0; b < layer1_size; b++)
+						x += cent[layer1_size * d + b] * vectors[c][b];
+					if (x > closev) {
+						closev = x;
+						closeid = d;
+					}
+				}
+				cl[c] = closeid;
+			}
+			cout << a << endl;
 		}
+
+		//for (a = 0; a < vec_size; a++)
+		//	mWordEmbedding -> ChangeEmbedWordCl(IDvectors[a], cl[a]);
+		cout << "End Kmeans" << endl;
+	}
 
 	vector<real *> GetCentroid() {
-			// Return the centroid coordinates
-			vector<real *> result;
-			result.clear();
-			unsigned i;
-			for (i = 0; i < clcn; i++) {
-				real *centroid = new real[layer1_size];
-				for (int j = 0; j < layer1_size; j++)
-					centroid[j] = cent[i * layer1_size + j];
-				result.push_back(centroid);
-			}
-			return result;
+		// Return the centroid coordinates
+		vector<real *> result;
+		result.clear();
+		unsigned i;
+		for (i = 0; i < numClusters; i++) {
+			real *centroid = new real[layer1_size];
+			for (int j = 0; j < layer1_size; j++)
+				centroid[j] = cent[i * layer1_size + j];
+			result.push_back(centroid);
 		}
-
-	void GetAllEmbedding(Document *Doc) {
-		int cnt = 0, cntvalid = 0;
-		Doc -> CleanAllWord();
-		for (Umap :: iterator Uit = Doc->mWordCount.begin(); Uit != Doc->mWordCount.end(); Uit++) {
-
-			cnt ++;
-			Char *mWord = Doc -> GetWord(Uit -> first);
-            // Word in the document
-            int index = mWordEmbedding -> SearchVocab(mWord);
-            if (index == -1) continue;
-            // Word ID in the Embedding
-            cntvalid++;
-            Doc -> AddToAllWord(make_pair(Uit -> first, Uit -> second));
-            int AllIndex = Doc -> GetWordSize() - 1;
-            Embeds vec = new real[layer1_size];
-            real *mEmbed = (*mWordEmbedding).GetEmbedding(index);
-            for (int j = 0; j < Uit -> second; j++) {
-            	vectors.push_back(mEmbed);
-            	IDvectors.push_back(index);
-            }
-            for (int j = 0; j < layer1_size; j++)
-            	vec[j] = mEmbed[j];
-            Doc -> AddEmbed(vec);
-		}
-		printf("%d valid\n%d in total\n word contained ratio: %.6lf\n", cntvalid, cnt, (double)cntvalid / cnt);
-		printf("AllWord Size: %d\n", Doc -> GetWordSize());
+		return result;
 	}
+
+	int* GetLabels() {
+		return cl;
+	}
+
 };
+#endif
--- heads.h
+++ heads.h
@@ -1,5 +1,5 @@
-#ifndef _HEADS_H_
-#define _HEADS_H_
+#ifndef _TFPROJECT_HEADS_H_
+#define _TFPROJECT_HEADS_H_
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,6 +15,7 @@
 #include <pthread.h>
 #include <cstdint>
 #include <cassert>
+#include <iostream>
 
 #define MAX_THREADS 1
 #define DIS_INF 1000
@@ -30,6 +31,11 @@
 #define MAX_KEY_WORDS 2000
 #define QUERY_DOC 500
 #define THRESHOLD_K 50
+#define ITER 10
+#define vocab_hash_size 30000000 // Maximum 30 * 0.7 = 21M words in the vocabulary
+#define max_w 50   //dimensionality that represent document - cluster number
+#define layer1_size 300
+
 #define MAX_DOCS ((MAX_TAGS) * (MAX_DOC_PER_TAG))
 #define check printf("here is ok\n")
 #define debug(...) fprintf(stderr, __VA_ARGS__)
@@ -44,44 +50,5 @@ typedef real* Embeds;
 typedef vector<Embeds> AllEmbeds;
 
 
-char train_file[MAX_STRING], output_file[MAX_STRING];
-char save_vocab_file[MAX_STRING], read_vocab_file[MAX_STRING];
 
-int vocab_hash_size = 30000000;  // Maximum 30 * 0.7 = 21M words in the vocabulary
-int dict_hash_size = 100000;
-int binary = 0, cbow = 1, debug_mode = 2, window = 5, min_count = 5, min_reduce = 1;
-
-long long max_w = 50;
-long long vocab_max_size = 1000, vocab_size = 0, layer1_size = 100;
-long long train_words = 0, word_count_actual = 0, iter = 5, file_size = 0, classes = 0;
-real alpha = 0.025, starting_alpha, sample = 1e-3;
-real *syn0, *syn1, *syn1neg, *expTable;
-real **cost;
-
-char totalinput[] = "input.txt";
-
-
-int hs = 0, negative = 5;
-const int table_size = 1e8;
-int *table;
-real SQR(real x) {
-	return x * x;
-}
-real WordDistance(const Embeds &a, const Embeds &b) {
-	real result = 0;
-	if (a == NULL || b == NULL) return DIS_INF;
-	for (int i = 0; i < layer1_size; i++)
-		result += SQR(a[i] - b[i]);
-	return result;
-}
-void ReadWord(Char *word, FILE *f) {
-	int a = 0;
-	while (1) {
-		word[a] = fgetc(f);
-		if (!isalpha(word[a])) break;
-		if (isupper(word[a])) word[a] = word[a] - 'A' + 'a';
-		if ((a < max_w) && (word[a] != '\n')) a++;
-	}
-	word[a] = 0;
-}
 #endif
